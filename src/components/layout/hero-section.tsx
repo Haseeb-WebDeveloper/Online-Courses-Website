1bac95228d36b4dd244972a0e345221055401485
Haseeb Ahmed Raza Khan <web.dev.haseeb@gmail.com>
2025-05-04 10:11:44 +0500

    max-w fix

--- src/components/layout/hero-section.tsx
+++ src/components/layout/hero-section.tsx
@@ -93,8 +93,8 @@ export function HeroSection() {
             transition={{ duration: 0.5 }}
             className="relative"
           >
-            <h1 className="text-4xl md:text-6xl font-custom max-w-6xl mx-auto leading-normal ">
-              <span className="">
+            <h1 className="text-4xl md:text-6xl font-custom max-w-5xl mx-auto  ">
+              <span className="leading-[130%]">
               Osiągnij wymarzoną wolność finansowa dzięki sprawdzonym strategiom
               </span>
             </h1>
@@ -106,7 +106,7 @@ export function HeroSection() {
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5, delay: 0.2 }}
-              className="text-lg md:text-xl text-muted-foreground max-w-4xl mx-auto"
+              className="text-lg md:text-xl text-muted-foreground max-w-2xl mx-auto"
             >
               Bez zbędnych informacji, samo mięso. Tylko sprawdzone strategie przetestowane przez setki ludzi
             </motion.p>
